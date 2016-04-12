7658b3cde13e847aa2f50f41b9644a825d02e345
Abdullah Ilgaz <mail@abdullahilgaz.com>
2016-04-12 18:46:34 +0300

    Added files via upload

---
+++ ITest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arches.Models;
+
+namespace Arches.BusinessLogic.Abstract
+{
+    public interface ITest
+    {
+		Test Find(Test test);
+        List<Test> FindAll();
+        void Insert(Test test);
+        void Update(Test test);
+        bool Delete(int id);
+    }
+}
