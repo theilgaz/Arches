f2f9d9dc4771998ab6776840a3e999bbc9a9e6e8
Abdullah Ilgaz <mail@abdullahilgaz.com>
2016-04-12 18:49:17 +0300

    Create Test.cs

---
+++ ArchesFramework/Arches.Models/Test.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Arches.Models
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public string Col1 { get; set; }
+        public string Col2 { get; set; }
+        public string Col3 { get; set; }
+    }
+}
